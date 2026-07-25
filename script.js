7089a704a976a228971d8e610d5a192c57ca329e
huutb <huutb94@gmail.com>
2026-07-25 09:16:25 +0700

    Refactor app display structure in script.js

--- script.js
+++ script.js
@@ -3,12 +3,8 @@ const list = document.getElementById("app-list");
 apps.forEach(app => {
     list.innerHTML += `
     <div class="app">
-        <div class="info">
-            <h3>${app.name}</h3>
-            <p>Phiên bản: ${app.version}</p>
-            <p>Dung lượng: ${app.size}</p>
-        </div>
-        <a class="btn" href="${app.file}" download>Tải xuống</a>
+        <span class="name">${app.name}</span>
+        <a href="${app.file}" class="btn" download>Tải xuống</a>
     </div>
     `;
 });
