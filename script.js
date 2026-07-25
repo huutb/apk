a4459bcf4abf965c2b74a75165a8f2744871d9b5
huutb <huutb94@gmail.com>
2026-07-25 09:11:24 +0700

    Update app download link text in script.js

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 const list = document.getElementById("app-list");
 
 apps.forEach(app => {
-  list.innerHTML += `
+    list.innerHTML += `
     <div class="app">
-        <img src="${app.icon}" class="icon">
         <div class="info">
             <h3>${app.name}</h3>
             <p>Phiên bản: ${app.version}</p>
             <p>Dung lượng: ${app.size}</p>
         </div>
-        <a href="${app.file}" class="btn" download>Tải</a>
+        <a class="btn" href="${app.file}" download>Tải xuống</a>
     </div>
-  `;
+    `;
 });
