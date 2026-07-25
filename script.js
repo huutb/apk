d7b795e5c6093db3f3a6b37581acda676ce94c93
huutb <huutb94@gmail.com>
2026-07-25 09:25:02 +0700

    Change span to div for app name display

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const list = document.getElementById("app-list");
 apps.forEach(app => {
     list.innerHTML += `
     <div class="app">
-        <span class="name">${app.name}</span>
+        <div class="name">${app.name}</div>
         <a href="${app.file}" class="btn" download>Tải xuống</a>
     </div>
     `;
