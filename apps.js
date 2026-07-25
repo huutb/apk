1ba6739cd2b4dcac41d4823291c4b2930f7a6d97
huutb <huutb94@gmail.com>
2026-07-25 09:19:14 +0700

    Add new apps to the apps array

--- apps.js
+++ apps.js
@@ -2,5 +2,13 @@ const apps = [
   {
     name: "ZArchiver",
     file: "apk/ZArchiver.apk"
+  },
+  {
+    name: "APKPure",
+    file: "apk/APKPure.apk"
+  },
+  {
+    name: "Revanced",
+    file: "apk/Revanced.apk"
   }
 ];
