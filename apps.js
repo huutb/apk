8f03063fd33deeedac5022034dd939350ef4e4fd
huutb <huutb94@gmail.com>
2026-07-25 08:55:04 +0700

    Add app details for ZArchiver and APKPure

--- apps.js
+++ apps.js
@@ -1,1 +1,16 @@
-
+const apps = [
+  {
+    name: "ZArchiver",
+    version: "1.0.10",
+    size: "8.5 MB",
+    icon: "icon/zarchiver.png",
+    file: "apk/ZArchiver.apk"
+  },
+  {
+    name: "APKPure",
+    version: "3.20",
+    size: "18 MB",
+    icon: "icon/apkpure.png",
+    file: "apk/APKPure.apk"
+  }
+];
