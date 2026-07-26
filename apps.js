5ca8bf3470d0853b8b50f81bb32d3a9bf0380f9a
huutb <huutb94@gmail.com>
2026-07-26 17:25:55 +0700

    Fix syntax error in apps.js

--- apps.js
+++ apps.js
@@ -10,7 +10,7 @@ const apps = [
   {
     name: "Revanced",
     file: "https://drive.google.com/file/d/1N9Ik6_-J4kzfYu5fIFk6YqpI0N4n6mt7/view?usp=drivesdk"
-  }
+  },
   {
     name: "MicroG",
     file: "https://drive.google.com/file/d/1OhRiLccGe88KtVDQMlhQCLy6GUTFnnfn/view?usp=drivesdk"
