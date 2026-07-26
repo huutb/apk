f6c21edebd028107b6355dea1b42b7739eb3c18f
huutb <huutb94@gmail.com>
2026-07-26 18:26:17 +0700

    Refactor app list rendering in script.js

--- script.js
+++ script.js
@@ -6,20 +6,15 @@ fetch("apps.json", {
   const list = document.getElementById("app-list");
 
   apps.forEach(app => {
+    const isExternal = app.file.startsWith("http");
+
     list.innerHTML += `
       <div class="app">
         <div class="name">${app.name}</div>
-        const isExternal = app.file.startsWith("http");
-
-list.innerHTML += `
-<div class="app">
-  <div class="name">${app.name}</div>
-  <a href="${app.file}"
-     class="btn"
-     ${isExternal ? 'target="_blank" rel="noopener"' : 'download'}>
-     Tải xuống
-  </a>
-</div>`;
+        <a href="${app.file}" class="btn"
+           ${isExternal ? 'target="_blank" rel="noopener"' : 'download'}>
+          Tải xuống
+        </a>
       </div>
     `;
   });
