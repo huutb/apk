18c072a27ccd61105f0020d761fa5ec3f0a824a4
huutb <huutb94@gmail.com>
2026-07-26 17:56:16 +0700

    Fetch apps data and update app list rendering

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-const list = document.getElementById("app-list");
+fetch("apps.json", {
+  cache: "no-store"
+})
+.then(res => res.json())
+.then(apps => {
+  const list = document.getElementById("app-list");
 
-apps.forEach(app => {
+  apps.forEach(app => {
     list.innerHTML += `
-    <div class="app">
+      <div class="app">
         <div class="name">${app.name}</div>
         <a href="${app.file}" class="btn" download>Tải xuống</a>
-    </div>
+      </div>
     `;
+  });
 });
